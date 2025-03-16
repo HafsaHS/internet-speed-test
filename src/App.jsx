67e9a51b4d023c27c72dce9c1300c98605c13620
Hafsa Hassan <hafsasiddiqui94@gmail.com>
2025-03-17 01:16:45 +0500

    update index.html to link App.css; add speed test functionality using @cloudflare/speedtest; integrate chart.js and react-chartjs-2 for data visualization; adjust CSS imports and clean up App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import SpeedTest from "@cloudflare/speedtest";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [speedTestResults, setSpeedTestResults] = useState();
+  const [downloadLatency, setDownloadLatency] = useState([]);
+
+  const config = {
+    measureDownloadLoadedLatency: true,
+    autoStart: false,
+    measurements: [
+      { type: "latency", numPackets: 1 }, // initial latency estimation
+      { type: "download", bytes: 1e5, count: 1, bypassMinDuration: true }, // initial download estimation
+      { type: "latency", numPackets: 20 },
+      { type: "download", bytes: 1e5, count: 9 },
+      { type: "download", bytes: 1e6, count: 8 },
+      { type: "upload", bytes: 1e5, count: 8 },
+      { type: "packetLoss", numPackets: 1e3, responsesWaitTime: 3000 },
+      { type: "upload", bytes: 1e6, count: 6 },
+      { type: "download", bytes: 1e7, count: 6 },
+      { type: "upload", bytes: 1e7, count: 4 },
+      { type: "download", bytes: 2.5e7, count: 4 },
+      { type: "upload", bytes: 2.5e7, count: 4 },
+      { type: "download", bytes: 1e8, count: 3 },
+      { type: "upload", bytes: 5e7, count: 3 },
+      { type: "download", bytes: 2.5e8, count: 2 },
+    ],
+  };
+
+  const safeFetch = async (url, options = {}) => {
+    try {
+      const request = await fetch(url, options);
+      const json = JSON.parse(await request.text());
+      const headers = request.headers;
+      return { json, headers };
+    } catch (_) {
+      return {};
+    }
+  };
+
+  const runSpeedTest = async () => {
+    const ua = { user_agent: window.navigator.userAgent };
+    const meta = (await safeFetch("https://speed.cloudflare.com/meta")).json;
+    const now = new Date();
+    const ts = { 
+      epoch: Math.floor(now.getTime() / 1000),
+      dateTime: now.toISOString()
+    };
+
+    const engine = new SpeedTest(config);
+
+    engine.onResultsChange = (results) => {
+      const summary = engine.results.getSummary();
+      const scores = engine.results.getScores();
+      const downloadedLatency = engine.results.getDownLoadedLatency();
+      setSpeedTestResults({ ...scores, ...summary, ...meta, ...ts, ...ua });
+      console.log(speedTestResults);
+      setDownloadLatency(downloadedLatency);
+      console.log(downloadLatency);
+      
+    };
+
+    engine.onFinish = (results) => {
+      const summary = results.getSummary();
+      const scores = results.getScores();
+      const downloadedLatency = results.getDownLoadedLatency();
+
+
+      setSpeedTestResults({ ...scores, ...summary, ...meta, ...ts, ...ua });
+      console.log(speedTestResults);
+      const finishedElement = document.createElement("div");
+      finishedElement.id = "speedtest-finished";
+      document.body.appendChild(finishedElement);
+      console.log("done");
+      console.log(results.getDownloadBandwidth());
+      console.log(results.getDownLoadedLatency());
+    };
+
+    console.log("running");
+
+    engine.play();
+  };
+
+  useEffect(() => {
+    runSpeedTest();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        {speedTestResults && (
+          <div>
+            <h1>Speed Test Results:</h1>
+            <pre>{JSON.stringify(speedTestResults, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(speedTestResults.download.latency, null, 2)}</pre> */}
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="mb-5 bg-amber-400">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
